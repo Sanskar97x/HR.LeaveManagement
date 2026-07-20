19ab5d0ea1859547499f1612e72a4d1be86f3f61
Sanskar97x <tamangsanskar3@gmail.com>
2026-07-20 21:45:37 +0545

    Interface For Leave Request

---
+++ HR.LeaveManagement.Application/Persistance/Contracts/ILeaveRequestRepository.cs
@@ -0,0 +1,12 @@
+﻿using HR.LeaveManagement.Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HR.LeaveManagement.Application.Persistance.Contracts
+{
+    public interface ILeaveRequestRepository : IGenericRepository<LeaveRequest>
+    {
+
+    }
+}
