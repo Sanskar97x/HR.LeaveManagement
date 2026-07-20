a5786c1ceefed16e5f87e5bca438b611ca4505c8
Sanskar97x <tamangsanskar3@gmail.com>
2026-07-21 04:56:42 +0545

    Model For Leave Allocation Added

---
+++ HR.LeaveManagement.Domain/LeaveAllocation.cs
@@ -0,0 +1,16 @@
+﻿
+using HR.LeaveManagement.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HR.LeaveManagement.Domain
+{
+    public class LeaveAllocation : BaseDomainEntity
+    {
+        public int NumberOfDays { get; set; } = 0;
+        public LeaveType LeaveType { get; set; } = new LeaveType();
+        public int LeaveTypeId { get; set; }
+        public int Period { get; set; }
+    }
+}
