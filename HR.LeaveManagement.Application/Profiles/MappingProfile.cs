a42ac19a38b8ffc1ad2121cbf88cf81650e4a5d1
Sanskar97x <tamangsanskar3@gmail.com>
2026-07-20 21:49:40 +0545

    Mapping Profile Added

---
+++ HR.LeaveManagement.Application/Profiles/MappingProfile.cs
@@ -0,0 +1,11 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HR.LeaveManagement.Application.Profiles
+{
+    public class MappingProfile : Profile
+    {
+    }
+}
