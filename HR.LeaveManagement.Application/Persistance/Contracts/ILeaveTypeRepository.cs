a2392df35e7da92cb2f1aa0802cf1a3b5f22e33a
Sanskar97x <tamangsanskar3@gmail.com>
2026-07-20 21:46:13 +0545

    Interface For Leave Type

---
+++ HR.LeaveManagement.Application/Persistance/Contracts/ILeaveTypeRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using HR.LeaveManagement.Domain;
+
+namespace HR.LeaveManagement.Application.Persistance.Contracts
+{
+    public interface ILeaveTypeRepository : IGenericRepository<LeaveType>
+    {
+    }
+}
