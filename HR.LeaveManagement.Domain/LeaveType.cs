de33af7a3e7710605f8c6f500f3b6be02e6c29c6
Sanskar97x <tamangsanskar3@gmail.com>
2026-07-22 11:15:48 +0545

    Model For Leave Type  Added

---
+++ HR.LeaveManagement.Domain/LeaveType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using HR.LeaveManagement.Domain.Common;
+
+namespace HR.LeaveManagement.Domain
+{
+    public class LeaveType : BaseDomainEntity
+    {
+        public string Name { get; set; } = string.Empty;
+        public int DefaultDays { get; set; }
+      
+    }
+}
