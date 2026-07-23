2cfe740269d7a7735569d027e6476420ca926099
Sanskar97x <tamangsanskar3@gmail.com>
2026-07-23 19:01:40 +0545

    LeaveAllocationDto Added

---
+++ HR.LeaveManagement.Application/DTOs/LeaveAllocationDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using HR.LeaveManagement.Application.DTOs.Common;
+using HR.LeaveManagement.Domain;
+
+namespace HR.LeaveManagement.Application.DTOs
+{
+    public class LeaveAllocationDto : BaseDto
+    {
+        public int NumberOfDays { get; set; } = 0;
+        public LeaveTypeDto LeaveType { get; set; } = new LeaveTypeDto();
+        public int LeaveTypeId { get; set; }
+        public int Period { get; set; }
+    }
+}
