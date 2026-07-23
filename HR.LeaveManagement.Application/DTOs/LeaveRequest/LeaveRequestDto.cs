512e2dfd251033cb1678f95d1abc7e30d8799857
Sanskar97x <tamangsanskar3@gmail.com>
2026-07-23 18:57:48 +0545

    LeaveRequest DTO Added

---
+++ HR.LeaveManagement.Application/DTOs/LeaveRequest/LeaveRequestDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using HR.LeaveManagement.Application.DTOs.Common;
+using HR.LeaveManagement.Domain;
+
+namespace HR.LeaveManagement.Application.DTOs.LeaveRequest
+{
+    public class LeaveRequestDto : BaseDto
+    {
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public LeaveTypeDto LeaveType { get; set; } = new LeaveTypeDto();
+        public int LeaveTypeId { get; set; }
+        public DateTime DateRequested { get; set; }
+        public string RequestComments { get; set; } = string.Empty;
+
+        public DateTime DateActioned { get; set; }
+        public bool? Approved { get; set; }
+
+        public bool Cancelled { get; set; }
+    }
+}
