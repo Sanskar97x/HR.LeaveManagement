309e42b3186ba82e9374043b281f0f8a3daa5163
Sanskar97x <tamangsanskar3@gmail.com>
2026-07-23 19:03:07 +0545

    LeaveTypeDto Added

---
+++ HR.LeaveManagement.Application/DTOs/LeaveTypeDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using HR.LeaveManagement.Application.DTOs.Common;
+
+namespace HR.LeaveManagement.Application.DTOs
+{
+    public class LeaveTypeDto : BaseDto
+    {
+        public string Name { get; set; } = string.Empty;
+        public int DefaultDays { get; set; }
+
+    }
+}
